80787d4b1d57ea9b3245839a7ac6088df8d6940b
fabolandry <fabolandry@hotmail.com>
2022-06-30 12:31:43 -0700

    Page Setup_03

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 
 
 const style = {
-  wrapper: `flex justify-around h-screen w-screen select-none bg-black text-white`
+  wrapper: `flex justify-between h-screen w-screen select-none bg-black text-white`
 }
 
 
 export default function Home(){
   return (
     <div className={style.wrapper}>
+      <div>
+        Launchpad
+      </div>
       <div>
         Launchpad
       </div>
