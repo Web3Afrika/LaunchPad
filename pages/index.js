8c9091c8c05d7eee8048c997939303e5ab876eae
fabolandry <fabolandry@hotmail.com>
2022-07-06 21:51:16 -0700

    Page Setup_04

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,40 @@
-
-
+import LeftAnnouncement from '../components/LeftAnnouncement';
+import RightAnnouncement from '../components/RightAnnouncement';
 const style = {
-  wrapper: `flex justify-between h-screen w-screen select-none bg-black text-white`
+  wrapper: `flex flex-col h-screen w-screen bg-black text-white overflow-scroll scrollbar-hide`,
+  header: `flex flex-row justify-between text-white`,
+  headerLeft: `flex flex-row ml-16 mt-10`,
+  headerRight: `flex flex-row items-center mr-20 mt-10`,
+  headerTitle: `font-specialelite text-4xl mt-6 ml-6`,
+  projectDescription : `flex flex-row self-center border-2 border-white mt-28 w-8/12 h-auto`,
+  launchpadCommunications : `flex flex-row justify-around mt-28 w-screen h-auto`,
+  footer: `flex flex-row self-center justify-center border-t mt-12 w-8/12 h-auto`
 }
 
 
 export default function Home(){
   return (
     <div className={style.wrapper}>
-      <div>
-        Launchpad
+      <div className={style.header}>
+        <div className={style.headerLeft}>
+          <img src="images/logo.jpg" />
+          <div className={style.headerTitle}>Launchpad</div>
+        </div>
+        <div className={style.headerRight}>
+          <div>
+            {/*Connect Wallet*/}
+          </div>
+        </div>
       </div>
-      <div>
-        Launchpad
+      <div className={style.projectDescription}>
+        {/*NFT marketplace + Project description*/}
+      </div>
+      <div className={style.launchpadCommunications}>
+        <LeftAnnouncement/>
+        <RightAnnouncement/>
+      </div>
+      <div className={style.footer}>
+        <img src="images/logo.jpg" />
       </div>
     </div>
   )
